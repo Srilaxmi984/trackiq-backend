149e34d2b975c5094b9cd1371dd3d39064677a3e
srilaxmi <srilaxmivallampatla9@gmail.com>
2026-06-01 00:09:56 +0530

    fixed cors for vercel frontend

--- src/main/java/com/trackiq/backend/config/SecurityConfig.java
+++ src/main/java/com/trackiq/backend/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://trackiq-frontend.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
